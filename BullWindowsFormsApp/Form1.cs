b38ba094f8c921d1d59ccdd6cd75cf142fa82962
LanaKo0507 <lanako0507@gmail.com>
2021-03-24 10:51:01 +0300

    сделала генерацию рандомного числа

--- BullWindowsFormsApp/Form1.cs
+++ BullWindowsFormsApp/Form1.cs
@@ -12,9 +12,16 @@ namespace BullWindowsFormsApp
 {
     public partial class Form1 : Form
     {
+        const int numbersLeght = 4;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            random.Next(0, numbersLeght);
         }
     }
 }
